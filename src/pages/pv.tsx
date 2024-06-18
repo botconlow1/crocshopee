f1b478839d362d287d7b84426d4747f45497dbf6
william borges <williamborgesaldrighii@gmail.com>
2024-06-17 22:54:00 -0300

    dsdsd

--- src/pages/pv.tsx
+++ src/pages/pv.tsx
@@ -60,6 +60,7 @@ export default function Home() {
         <meta name="description" content="APP WhatsPix" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo2.png" />
+        <script src="/latest.js" async defer></script>
       </Head>
 
       <MainContainer className={Vietnam.className}>
